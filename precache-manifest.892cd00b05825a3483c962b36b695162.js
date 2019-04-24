0635b1c7306911ec08fe17566b02e3e9730b2a21
ganyuanyuan <31822371+ganyuanyuan@users.noreply.github.com>
2019-04-24 15:20:56 -0400

    Updates

---
+++ precache-manifest.892cd00b05825a3483c962b36b695162.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65bf5fa69ffcb05a82fca2558b196638",
+    "url": "/robotfriend-webapplication-exercise/index.html"
+  },
+  {
+    "revision": "165c83f85a4584eb65d7",
+    "url": "/robotfriend-webapplication-exercise/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "53ac20a0f0cc28eb15f7",
+    "url": "/robotfriend-webapplication-exercise/static/css/main.a0809b31.chunk.css"
+  },
+  {
+    "revision": "165c83f85a4584eb65d7",
+    "url": "/robotfriend-webapplication-exercise/static/js/2.590933e2.chunk.js"
+  },
+  {
+    "revision": "53ac20a0f0cc28eb15f7",
+    "url": "/robotfriend-webapplication-exercise/static/js/main.4067adb8.chunk.js"
+  },
+  {
+    "revision": "514dd9cfe87ee7263858",
+    "url": "/robotfriend-webapplication-exercise/static/js/runtime~main.1e784c49.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robotfriend-webapplication-exercise/static/media/SEGA.12a3db8b.woff"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.0/workbox-sw.js");
 
 importScripts(
-  "ganyuanyuan.github.io/robotfriend-webapplication-exercise/precache-manifest.37b9ae08c639c72bce9565208d94c12d.js"
+  "/robotfriend-webapplication-exercise/precache-manifest.892cd00b05825a3483c962b36b695162.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("ganyuanyuan.github.io/robotfriend-webapplication-exercise/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/robotfriend-webapplication-exercise/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
