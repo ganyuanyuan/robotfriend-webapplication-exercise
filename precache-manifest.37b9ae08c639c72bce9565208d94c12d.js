450e827006d7d65fe1d29d1e9d185ddddb3145d2
ganyuanyuan <31822371+ganyuanyuan@users.noreply.github.com>
2019-04-24 15:16:36 -0400

    Updates

---
+++ precache-manifest.37b9ae08c639c72bce9565208d94c12d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4bfb7ccf60594679647b5e58440660f6",
+    "url": "ganyuanyuan.github.io/robotfriend-webapplication-exercise/index.html"
+  },
+  {
+    "revision": "165c83f85a4584eb65d7",
+    "url": "ganyuanyuan.github.io/robotfriend-webapplication-exercise/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "02c2a05b07285c76f612",
+    "url": "ganyuanyuan.github.io/robotfriend-webapplication-exercise/static/css/main.b0cd5634.chunk.css"
+  },
+  {
+    "revision": "165c83f85a4584eb65d7",
+    "url": "ganyuanyuan.github.io/robotfriend-webapplication-exercise/static/js/2.590933e2.chunk.js"
+  },
+  {
+    "revision": "02c2a05b07285c76f612",
+    "url": "ganyuanyuan.github.io/robotfriend-webapplication-exercise/static/js/main.bf1ab122.chunk.js"
+  },
+  {
+    "revision": "148f4889f9398eeb6780",
+    "url": "ganyuanyuan.github.io/robotfriend-webapplication-exercise/static/js/runtime~main.6747b056.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "ganyuanyuan.github.io/robotfriend-webapplication-exercise/static/media/SEGA.12a3db8b.woff"
+  }
+]);
