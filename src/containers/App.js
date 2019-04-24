47a606488bf80d9382af90ea86ed621c77861164
ganyuanyuan <31822371+ganyuanyuan@users.noreply.github.com>
2019-04-24 16:32:55 -0400

    improvements

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import CardList from './CardList.js';
-import SearchBox from './SearchBox.js';
+import CardList from '../components/CardList.js';
+import SearchBox from '../components/SearchBox.js';
 import './App.css';
-import Scroll from './Scroll.js';
+import Scroll from '../components/Scroll.js';
 
 
 class App extends React.Component{
@@ -23,13 +23,13 @@ class App extends React.Component{
   }
 
   render(){
-    const filteredRobots = this.state.robots.filter(robots =>{
-      return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+    const {robots, searchfield}= this.state;
+    const filteredRobots = robots.filter(robot =>{
+      return robot.name.toLowerCase().includes(searchfield.toLowerCase());
     })
-    if  (this.state.robots.length===0){
-      return <h1>Loading...</h1>
-    }else{
-      return (
+    return !robots.length ?
+      <h1>Loading...</h1> :
+      (
         <div className='tc'>
           <h1 className='f1'>RoboFriends</h1>
           <SearchBox searchChange={this.onSearchChange}/>
@@ -38,7 +38,6 @@ class App extends React.Component{
           </Scroll>
         </div>
       );
-    }
   }
 }
 
